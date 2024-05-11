b729b8dfc8b0fd45cf790db0824454829ac35d3b
dangnguyenngochai <68412631+dangnguyenngochai@users.noreply.github.com>
2024-05-12 00:57:19 +0700

    Add files via upload

---
+++ run_demo.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/bash
+
+HF_token="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";
+
+echo "Cloning MiniGPT4-video ...";
+
+git clone https://github.com/Vision-CAIR/MiniGPT4-video.git;
+cd MiniGPT4-video;
+
+echo "Done";
+
+echo "Creating Conda environment...";
+
+conda env create -f environment.yml;
+source activate minigpt4_video;
+
+echo "Done";
+
+echo "Downloading MetaAi llama model";
+
+git clone https://dangnguyenngochai:$HF_token@huggingface.co/Vision-CAIR/MiniGPT4-Video;
+
+echo "Done";
+
+echo "Login to hugging face...";
+
+huggingface-cli login --token $HF_token;
+
+echo "Done";
+
+echo "Start gradio demo...";
+
+python minigpt4_video_demo.py --ckpt MiniGPT4-Video/checkpoints/video_llama_checkpoint_last.pth --cfg-path test_configs/llama2_test_config.yaml;
