8d426e6d6cd6235af305d03a6892188533ba5332
dangnguyenngochai <68412631+dangnguyenngochai@users.noreply.github.com>
2024-05-12 13:59:09 +0700

    Create launch_gradio.sh

---
+++ launch_gradio.sh
@@ -0,0 +1,5 @@
+#! /usr/bash/bin
+
+cd  MiniGPT4-video;
+source activate minigpt4_video;
+python minigpt4_video_demo.py --ckpt MiniGPT4-Video/checkpoints/video_llama_checkpoint_last.pth --cfg-path test_configs/llama2_test_config.yaml --gradio_port 2411;
